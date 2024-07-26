fb6e7f3bb0dcc7601742675853faa44acdb67c7a
MarceloToccato <marcelo.martins@toccato.com.br>
2024-07-26 10:17:39 -0300

    Update GrafanaCreate.py

--- GrafanaCreate/GrafanaCreate.py
+++ GrafanaCreate/GrafanaCreate.py
@@ -1,3 +1,15 @@
+# O código a seguir tem a utilidade de criar um arquivo .json, com intuito de importalo para o grafana
+# Fazendo o grafana criar um dashboard já pronto
+
+
+
+
+
+
+
+
+Hostgrup = ''  # Altere para o grupo de host que seu host se encontra'
+hostname = '' # Altere para o nome do seu Host 
 
 
 
@@ -7,78 +19,36 @@
 
 
 
+#O código a baixo serve para  criar um ID unico que se localiza no código na linha 4015
+###
 
+import random as rm #Importando a biblioteca random
+code = [] #Criando uma array onde a O ID unico ficara
 
-
-Hostgrup = 'TOCCATO'
-hostname = 'TOCCATO - webprod01.toccato.local'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import random as rm
-code = []
+#Todos os caracteres que podem ser utilizados no ID unico
 Caracteres = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 
 
 
-for i in range (7): #loop para adicionar os caracteres aleatorios na array senha 
+for i in range (7): #Loop para adicionar os caracteres aleatorios na array code
          
-            i = i + 1 #essa linha de comando é para não deixar o loop infinito, ele faz parar quando o numero de caracteres desejados acontece
          
-            simbolos = rm.choice(Caracteres) #escolhendo caracter
+            simbolos = rm.choice(Caracteres) #Escolhendo caracter
          
-            code.append(simbolos)
+            code.append(simbolos) #Adiciona o caracter na array code
 
 
-code = (''.join(code))
+code = (''.join(code)) #Define que code é um texto tudo junto sem espaço entre os itens
+#Fazendo com que ele forme um ID unico
+
+###
 
 
+# Essa variável 'grafanajson' é um .json que caso importado cria um dashboard no grafana
+# Porém nos lugares onde se coloca o Hostgroup e o Hostname colocamos os seguintes comandos 
+# f'{Hostgrup} e f'{hostname} para importar as variávels já definidas
 
 
 grafanajson ={
@@ -4046,20 +4016,32 @@ grafanajson ={
   "version": 10,
   "weekStart": ""
 }
-import os
-import json
 
-folder_path = 'json'
-file_name = f'{hostname}'+'.json'
-file_path = os.path.join(folder_path, file_name)
+# Os códigos seguinte tem o intuito de:
+# Salvar a variavel grafanajson em um arquivo .json 
+# Abrir o explorador de arquivos onde o tal arquivo se encontra 
+
+import os #Importa uma biblioteca para conseguir mexer no sistema operacional via python
+import json #Importa uma biblioteca para conseguir mexer com json
+
+folder_path = 'json' #Define a pasta onde o arquivo será salvo
+
+file_name = f'{hostname}'+'.json' #Define o nome que o arquivo terá, juntando o nome do host com o .json
 
 
-with open (file_path,'w') as grafana:
-  json.dump(grafanajson, grafana, indent=2, default=str)
+file_path = os.path.join(folder_path, file_name) #cria o caminho onde o arquivo será salvo
 
-pasta = 'C:\\Users\\User\\Desktop\\projetos\\GrafanaCreate\\json'
 
-os.startfile(pasta)
+with open (file_path,'w') as grafana: #abre um arquivo onde a variavel a cima definiu
+  #escreve o conteudo da variável 'grafanajson'
+  json.dump(grafanajson, grafana)
+
+  #Caso o arquivo não esteja nesse direcionamente altere para onde a pasta com o código se encontra 
+diretorio = 'C:\\Users\\User\\Desktop\\Downloads\\GrafanaCreate\\json' #defina o diretório onde o json foi salvo  
+
+os.startfile(diretorio) 
+#abre o explorador de arquivos onde o json está. 
+#para apenas ter que arrastar o arquivo para a aba de importar do grafana
 
 
 
